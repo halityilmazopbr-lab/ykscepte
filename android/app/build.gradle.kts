edeaa048eec5fb0d37c3dee0d0c5cf45dab5131e
Halit Yilmaz <yilmaz@ykscepte.com>
2025-12-26 17:07:39 +0300

    Fix NDK version mismatch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ if (hasKeyProperties) {
 android {
     namespace = "com.ykscepte.app"
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
